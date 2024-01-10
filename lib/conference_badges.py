1d04781f85566d6d91d6d8ad7ca385d180f2a810
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-10 08:04:26 -0600

    Assignment Done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return str(f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+    # badges = []
+    # for name in names:
+    #     badges.append(badge_maker(name))
+    # return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i in range(len(names)):
+        room_assignments.append(f"Hello, {names[i]}! You'll be assigned to room {i+1}!")
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    room_assignments = assign_rooms(names)
+    for room_assignment in room_assignments:
+        print(room_assignment)
